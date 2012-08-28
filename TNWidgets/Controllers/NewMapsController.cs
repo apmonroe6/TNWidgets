1f65b3d78203cac9c7e535d161afdb55d85e1d85
Andrew Monroe <apmonroe6@gmail.com>
2012-08-28 10:30:24 -0400

    initial commit
    widgets for display in talent networks

---
+++ TNWidgets/Controllers/NewMapsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TNWidgets.Controllers
+{
+    public class NewMapsController : Controller
+    {
+        //
+        // GET: /NewMaps/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
